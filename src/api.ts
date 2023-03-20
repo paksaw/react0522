eab8543db65b032854218dcfd4c35a88eaa8533a
Kimeuimin <kimem0131@naver.com>
2023-03-20 13:44:47 +0900

    api연결

---
+++ src/api.ts
@@ -0,0 +1,8 @@
+const API_KEY = "9a3037cf1839c50b07a1db2cf57a5bf7";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
